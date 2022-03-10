91f90918549bd7a566f7de3614bf33218485fb6e
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-10 15:08:46 +0300

    commit code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - output to string
+ * main - outputs to string
  *
- * Description: prints out "and that piece of art is useful.."without printf
+ * Description: Prints out "and that piece of art is useful..." without printf
  *
  * Return: 1
  */
 int main(void)
 {
-	char *s="and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	long l = 59;
-	long fd= 1;
+	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
-	__asm__("syscall"
+	__asm__ ("syscall"
 			: "=a" (ret)
 			: "a" (syscall),
-			"D"(fd),
-			"S"(s),
-			"d"(l));
+			"D" (fd),
+			"S" (s),
+			"d" (l));
 	return (1);
 }
