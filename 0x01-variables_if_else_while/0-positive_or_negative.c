798a463f3a515c02c57262937add69d67bad6573
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-11 19:59:01 +0300

    Display the negativ or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Description: Display positive or negative
+ * Return: 0
+ */
 int main(void)
 {
-
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
-	{
-		printf("%d is Postive", n);
-	}
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else if (n < 0)
-	{
-		printf("%d is Negative", n);
-        }
-	else
-		printf("%d is Zero", n);
-
+		printf("%d is negative\n", n);
 	return (0);
 }
