963c77c3c3f49c58253150ebdc9b151f24d1e7eb
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-11 19:42:37 +0300

    If else display

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,25 @@
 #include <stdlib.h>
-
 #include <time.h>
-/**
- * main - main - block
- * Get a random number and print the number
- * and if postive, negative, or zero
- * Return: 0
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0){
+	/* your code goes there */
+	if (n > 0)
+	{
 		printf("%d is Postive", n);
-	}
-	else if (n < 0){
-	printf("%d is Negative", n);
-	}
+        }
+	else if (n < 0)
+	{
+		printf("%d is Negative", n);
+        }
 	else
-	printf("%d is Zero", n);
+		printf("%d is Zero", n);
+
 	return (0);
 }
