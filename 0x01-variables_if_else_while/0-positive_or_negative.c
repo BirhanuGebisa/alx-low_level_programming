6e8ae4310a40b9d2969d8f227b9c669dd646fbc4
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-11 19:38:04 +0300

    If else display

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,12 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	printf("%d is Postive", n);
-	else if (n < 0)
+	if (n > 0){
+		printf("%d is Postive", n);
+	}
+	else if (n < 0){
 	printf("%d is Negative", n);
+	}
 	else
 	printf("%d is Zero", n);
 	return (0);
