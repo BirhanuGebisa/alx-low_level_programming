5947ba60d3e3587e162edd02b0651f6226e3cb99
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-11 19:56:16 +0300

    Display alphabet letter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- * main - entry point
- * Description: print alphabets
- * Return: 0 Success
- */
+* main - Entry code
+* Description: prints "last digit"
+* Return: 0
+*/
 int main(void)
 {
-	char c = 'a';
+	int n;
+	int last;
 
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	putchar('\n');
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
