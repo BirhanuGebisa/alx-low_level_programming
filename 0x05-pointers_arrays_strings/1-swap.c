a7a4b3540db50b713f2a44f3530e31fe5d2f3147
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-21 20:08:34 +0300

    Swap the numbers pointer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* swap_int - swaps the values
+*@a: pointer to a
+*@b: ponter to b
+*Return: Nothing
+*/ 
 void swap_int( int *a, int *b)
 {
 	int aux;
