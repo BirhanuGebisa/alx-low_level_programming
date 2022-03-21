688940c1252c345dc2ecdde08a4a77a15aa3cedd
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-21 19:56:00 +0300

    declare pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * update the variable pointers 
- * Return:0 Always 
+ *update the variable pointers 
+ *Return:0 Always 
  *
  */
 void reset_to_98(int *n)
 {
-	*n= 98;
+	*n = 98;
 }
