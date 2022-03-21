48a508b6f1c5152c1f22b2ed6082561bc24fe973
Birhanu Gebisagit <birhanugebisa@gmail.com>
2022-03-21 20:01:40 +0300

    declare pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - update the variable pointers 
- *@n: Pointers of the direction at the variable n
- * Return: 0 Always 
- */
+* reset_to_98 - updates the value it points
+*@n: pointer of the direcction at the variable n
+*Return: 0 (success)
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
