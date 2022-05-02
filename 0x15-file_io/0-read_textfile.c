ef1cd0269342e36c3db623ffa40a08565ff28408
Birhanu Gebisa <birhanugebisa@gmail.com>
2022-05-02 23:52:40 +0300

    converts a binary number to unsigned int

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - converts a binary number to unsigned int
- * @filename: pointer to string
- * @letters: string containing the binary number
- * Return: the converted number
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nletters;
 	int fd;
-	char *text;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 		return (0);
-	text = malloc(sizeof(char) * letters + 1);
-	if (text == NULL)
-		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
-	{
-		free(text);
 		return (0);
-	}
-	nletters = read(fd, text, sizeof(char) * letters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(fd);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
-	nletters = write(STDOUT_FILENO, text, nletters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(fd);
-		return (0);
-	}
-	free(text);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	return (nletters);
+
+	free(buf);
+
+	return (nwr);
 }
